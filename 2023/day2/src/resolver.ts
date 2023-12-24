3fbc25a37382a5092da281c9c46a8945b9c56be4
Philippe Gibert <philippe.gibert@gmail.com>
2023-12-24 07:52:22 +0100

    day2 : part2

--- 2023/day2/src/resolver.ts
+++ 2023/day2/src/resolver.ts
@@ -15,14 +15,14 @@ const extractGame = (line: string): { id: number, result: { [key: string]: numbe
             p[color] = (p[color] || 0) + parseInt(value, 10);
             return p;
         }, {})
-        
+
         Object.keys(prev).forEach(k => {
-            prev[k] = Math.max(prev[k], launches[k] || 0) 
+            prev[k] = Math.max(prev[k], launches[k] || 0)
         });
 
         return prev
 
-    }, {red: 0, green: 0, blue: 0})
+    }, { red: 0, green: 0, blue: 0 })
 
     return { id: parseInt(id, 10), result };
 }
@@ -31,11 +31,20 @@ const extractGame = (line: string): { id: number, result: { [key: string]: numbe
     const filePath = program.opts().file;
     const fileContent = readFileSync(filePath, 'utf8');
     const lines = fileContent.split('\n');
-    const result = lines.map(l => extractGame(l))
-        .filter(({ result }) => result.red <= 12 && result.green <= 13 && result.blue <= 14)
+
+    const data = lines.map(l => extractGame(l))
+
+    const result1 = data.filter(({ result }) => result.red <= 12 && result.green <= 13 && result.blue <= 14)
         .reduce((prev, { id }) => {
             return prev + id
 
         }, 0)
-    console.info(`result: ${result}`);
+    console.info(`result part 1: ${result1}`);
+
+    const result2 = data.reduce((prev, { result }) => {
+        const value = Object.keys(result).reduce((p, k) => result[k] * p, 1)
+        return prev + value;
+    }, 0)
+    console.info(`result: ${result2}`);
+
 })();
